25e4aaa93ff6f5c171eef847aba5765eeb0a3125
Maksim Misak <you@example.com>
2022-07-25 23:01:51 +0300

    created class planner to write to a CSV file

--- planner.java
+++ planner.java
@@ -1,5 +1,28 @@
 package planner;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
 public class planner {
-    
+
+    public void createPlanner(List<task> tasks) {
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(new File("Planner.csv"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String columns = "id, author's name, task creation date, task creation time, deadline";
+
+        pw.write(columns + "\n");
+
+        for (task task : tasks) {
+            pw.write(task.toString() + "\n");
+        }
+
+        pw.close();
+    }
+
 }
