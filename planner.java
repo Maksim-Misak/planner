68fcba9b2fd9ce28f55d9952d1e57b57b2d9462d
Maksim Misak <you@example.com>
2022-07-30 16:01:48 +0300

    moved intitializing of object task to planner instead of UI

--- planner.java
+++ planner.java
@@ -3,11 +3,18 @@ package planner;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class planner {
 
-    public void createPlanner(List<task> tasks) {
+    List<task> tasks = new ArrayList<>();
+
+    public void addTask(task task) {
+        tasks.add(task);
+    }
+
+    public void printPlanner() {
         PrintWriter pw = null;
         try {
             pw = new PrintWriter(new File("Planner.csv"));
